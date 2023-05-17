2791390ff2deda962219daf6f1c2ce45d9409b07
Aspen Wilson <aspen.schell@gmail.com>
2023-05-16 18:22:15 -0700

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word= word
+
+    def match(self, word_list):
+        anagrams = []
+        for i in word_list:
+            if sorted(self.word.lower()) == sorted(i.lower()) and self.word.lower() != i.lower():
+                anagrams.append(i)
+        return anagrams
